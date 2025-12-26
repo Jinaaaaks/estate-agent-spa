7a51c133fced81e15dabf6f1531bce073fbebdbb
Jinalee <jinaleesubasinghe16@gmail.com>
2025-12-26 13:19:17 +0530

    feat: add tabbed sections to property page

--- src/pages/PropertyPage.jsx
+++ src/pages/PropertyPage.jsx
@@ -1,5 +1,8 @@
 import { Link, useParams } from "react-router-dom";
 import properties from "../data/properties.json";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+
 
 export default function PropertyPage() {
   const { id } = useParams();
@@ -34,11 +37,41 @@ export default function PropertyPage() {
         <strong>Date added:</strong> {property.dateAdded}
       </p>
 
-      <h2>Short description</h2>
-      <p>{property.shortDescription}</p>
+      <Tabs>
+        <TabList>
+            <Tab>Description</Tab>
+            <Tab>Floor Plan</Tab>
+            <Tab>Map</Tab>
+        </TabList>
 
-      <h2>Long description</h2>
-      <p>{property.longDescription}</p>
+        <TabPanel>
+            <h2>Description</h2>
+            <p>{property.longDescription}</p>
+        </TabPanel>
+
+        <TabPanel>
+            <h2>Floor Plan</h2>
+            <img
+            src={property.floorPlan}
+            alt="Floor plan"
+            style={{ maxWidth: "100%", border: "1px solid #ccc" }}
+            />
+        </TabPanel>
+
+        <TabPanel>
+            <h2>Location</h2>
+            <iframe
+            title="map"
+            width="100%"
+            height="300"
+            style={{ border: 0 }}
+            loading="lazy"
+            allowFullScreen
+            src="https://www.google.com/maps?q=London&output=embed"
+            />
+        </TabPanel>
+      </Tabs>
+
     </main>
   );
 }
